6e82941cb00fa0ec90317b624c16bf6ca10ecefc
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-02-17 19:02:03 -0500

    02 17 2025
    just starting this next one up.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather Bservation Station 20
+ This is Weather Observation Station 20 from the website Hacker Rank dot com.  
---
+++ Weather Observation Station 20.sql
@@ -0,0 +1,5 @@
+--02 17 2025----------------------------------------
+
+--ok,
+
+--just starting this up for the next time I pick this up.
