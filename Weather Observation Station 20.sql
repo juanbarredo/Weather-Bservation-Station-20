2b980b5f84324738d73c2fe9e13131661ddb2c2c
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-02-23 11:31:49 -0500

    02 23 2025
    I realized that this is a problem that working through as deliciously as possibly will be worth it weight in gold.
    
    I need to really tackle this one.
    
    seems there is no Median function in sql.  the race is on.

--- Weather Observation Station 20.sql
+++ Weather Observation Station 20.sql
@@ -92,7 +92,86 @@ SELECT MAX(LAT_N) AS 'MAX(LAT_N)'
 --I have the field
 --that has
 --a higher half
---and a lowe half.
+--and a lower half.
 
 --now,
 --I need to isolate that number specifically.
+
+------------------------------------02 23 2025 ----------------------------
+
+--I am wondering if there isn't just a median function?
+
+--or I can count the number of rows.  noting that they are organized as such.
+
+--how do I count?
+
+--i think there is a literal function called count(column)
+
+USE Weather_Observation_Station_2;
+SELECT COUNT(LAT_N) AS 'COUNT(LAT_N)'
+	FROM STATION;
+	--would this have to be the subquery?
+
+--USE Weather_Observation_Station_2;
+--SELECT LAT_N AS 'MEDIAN'
+--	FROM STATION
+--	WHERE LAT_N IN
+--	(SELECT COUNT(LAT_N)
+--		FROM STATION);
+		--This is wrong.
+		--I need to do something that is quite out of my reach at the moment.
+
+		--I need to devide the count in half.
+		--I am now being flooded with how many more steps my query needs
+
+		--a median isn't just half plus one or minus one.
+
+		--ok, there isn't a function in it.
+
+		--very well.
+		--this is indeed one that I have to focus through.
+
+		--take my time.
+
+--next I need to build up a sense for that number.
+--I need to look at the mathematical formula.
+
+--yeah.  I reviewed a quick google search and it just splashed me.
+
+--ok.
+
+--this is definitely doable but it is now a marathon.
+
+--I have to check how to work with count.
+--I think count is my best friend.
+--as of right now I have the first step.
+--order them Descindingly.
+--I have also counted them in this order.
+
+--But this could be done in either order?
+
+--let's see.
+
+
+--the next step I have to do is to decide how to tackle the
+--if odd, perfect!
+--if even add the plus one and the minus one.
+
+--actually.
+
+--ok, so there are three steps.
+
+--I have done the first one.
+--the second one is the compound one.
+
+--identify through odd even number.
+
+--but I keep wanting to generalize.
+
+--i think then the next step is how to identify and process odd or even.
+
+--have one query for odd and have one query for even.
+
+--i imagine the subquery will hadle the odd or even.
+
+--can a subquery have a subquery?
