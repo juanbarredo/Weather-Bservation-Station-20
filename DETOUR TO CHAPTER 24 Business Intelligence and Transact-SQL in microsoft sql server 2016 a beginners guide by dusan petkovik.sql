f6559fcf24bc5ed1c4903dc77020467cbf26d6b6
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-03-06 19:52:35 -0500

    DETOUR to CHAPTER 24
    Going to see if taking a detour will lead me to thinking up a better answer.
    
    I am honestly stuck at how to get the median.

---
+++ DETOUR TO CHAPTER 24 Business Intelligence and Transact-SQL in microsoft sql server 2016 a beginners guide by dusan petkovik.sql
@@ -0,0 +1,38 @@
+---------------------------03 06 2025-------------------------
+--I am going to type this out instead of using the mindmapping software.
+
+--I think I don't need to map to get a layout.
+
+--I need to hit the weeds right now.
+
+--The SQL standard has adopted a set of online analytical processing (OLAP) f
+--functions that enable you to easily perdomr these calculations and many others
+--that used to be very complex for implementation.
+
+--this part of the SQL standar is called SQL/OLAP 
+
+--therefore, 
+--SQL/OLAP comprises all functions and operators that are used for data analysis.
+
+--using OLAP functions has several advantages fro users:
+
+----users with standard knwledge of SQL can easily specify the calculations they need.
+
+----databasae systems, such as the database engine, can perform these calculations more efficiently.
+
+----because there is a standard specification of these functions,
+----they're now much more economical for tool and application vendors to exploit.
+
+----almost all analytical functions proposed by the SQL standard are implemented in enterprise
+----database systems int he same way.
+----for this reason, 
+----you can port queries in relation to SQL/OLAP from one system to another,
+----without any code changes.
+
+--so, 
+
+--I am feeling frustrated that I wasted my attention span on something that I am hoping pays off.
+
+--we shall see.
+
+--i am honestly stuck otherwise.
