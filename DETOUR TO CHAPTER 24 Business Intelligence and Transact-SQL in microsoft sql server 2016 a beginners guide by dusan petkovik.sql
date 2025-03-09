0e72d35bc5b7dcf66ae984bca0930a51a4b10f21
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-03-09 11:31:49 -0400

    03 09 2025
    ok, so just continuing to explore Chapter 24.  Really excited for the possibilities once I continue to learn about the "Window Construct".
    
    It is a really complicated little subject.

--- DETOUR TO CHAPTER 24 Business Intelligence and Transact-SQL in microsoft sql server 2016 a beginners guide by dusan petkovik.sql
+++ DETOUR TO CHAPTER 24 Business Intelligence and Transact-SQL in microsoft sql server 2016 a beginners guide by dusan petkovik.sql
@@ -1,3 +1,9 @@
+--standard:
+
+-- means me.
+
+------------ means me copying from the book.
+
 ---------------------------03 06 2025-------------------------
 --I am going to type this out instead of using the mindmapping software.
 
@@ -5,29 +11,29 @@
 
 --I need to hit the weeds right now.
 
---The SQL standard has adopted a set of online analytical processing (OLAP) f
---functions that enable you to easily perdomr these calculations and many others
---that used to be very complex for implementation.
+-----------------The SQL standard has adopted a set of online analytical processing (OLAP) f
+---------------functions that enable you to easily perdomr these calculations and many others
+--------------that used to be very complex for implementation.
 
---this part of the SQL standar is called SQL/OLAP 
+------------------this part of the SQL standar is called SQL/OLAP 
 
---therefore, 
---SQL/OLAP comprises all functions and operators that are used for data analysis.
+---------------therefore, 
+----------------SQL/OLAP comprises all functions and operators that are used for data analysis.
 
---using OLAP functions has several advantages fro users:
+----------------using OLAP functions has several advantages fro users:
 
-----users with standard knwledge of SQL can easily specify the calculations they need.
+----------------users with standard knwledge of SQL can easily specify the calculations they need.
 
-----databasae systems, such as the database engine, can perform these calculations more efficiently.
+-----------------databasae systems, such as the database engine, can perform these calculations more efficiently.
 
-----because there is a standard specification of these functions,
-----they're now much more economical for tool and application vendors to exploit.
+-------------------because there is a standard specification of these functions,
+------------------they're now much more economical for tool and application vendors to exploit.
 
-----almost all analytical functions proposed by the SQL standard are implemented in enterprise
-----database systems int he same way.
-----for this reason, 
-----you can port queries in relation to SQL/OLAP from one system to another,
-----without any code changes.
+-------------------almost all analytical functions proposed by the SQL standard are implemented in enterprise
+------------------database systems int he same way.
+-----------------for this reason, 
+-----------------you can port queries in relation to SQL/OLAP from one system to another,
+-----------------without any code changes.
 
 --so, 
 
@@ -36,3 +42,59 @@
 --we shall see.
 
 --i am honestly stuck otherwise.
+
+--i want to describe my problem.
+
+--I want the selection to bring up only the median to add up.
+--i need to do the steps.
+--orgarnize by desc or asc.
+--then just count to the middle point.
+--if middle point is not a whole number.
+--then take two and add them together then divide by two.
+--otherwise.   present the number in the middle of the list.
+
+--so right now then.
+
+--i am working on counting to the middle point.
+--i am hoping that the "Window Construct"
+--will get me to count to the middle point?
+
+-----------------------------------------03 09 2025---------------------------------------------
+
+--I am going to continue working on the window construct.
+
+-------The database engine offers many extensions to the SELECT statement that 
+-------can be used primarily for analytic operations.
+-------some of these extensions area defined accoording ot he SQL standard and some are not.
+-------the following sections describe both standard and nonstandard SQL/OLAP functions and operators.
+
+-------WINDOW CONSTRUCT
+
+-------A window (in relation to SQL/OLAP) DEFINES A PARTITIONED SET OF ROWS TO WHICH A FUCTION IS APPLIED.
+-------the number of rows that belong to a window is dynamically determined in relation to the user's specifications.
+-------the window construct is specified using the OVER clause.
+
+-------the standard window construct has three main parts.
+
+--------partitioning 
+---------ordering and framing
+--------aggregation grouping 
+
+--right now "partitioning" is excting
+--"ordering and framing" are mysterious
+--"aggregation grouping" is legendary.
+
+----------before you delve into the window construct and its parts,
+----------take a look at the table that will be used for the examples.
+--ok, I already have that table.
+--let me get it out.
+
+USE sample
+SELECT *
+	FROM project;
+
+--ok, 
+--I thought I already had this table but this is a new table indeed.
+--I am unsure how to proceed.
+--in saving the table.
+--I think it is the sample database.
