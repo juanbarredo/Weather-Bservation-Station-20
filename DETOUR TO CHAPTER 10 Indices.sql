abd47b63071441d36f4fda3d7460079cb90d5362
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-03-23 12:35:32 -0400

    03 23 2025
    I explored chapter 10 on indices and got many many awesome ideas.

---
+++ DETOUR TO CHAPTER 10 Indices.sql
@@ -0,0 +1,65 @@
+-----------------------------------------03 23 2025---------------------------------
+
+----ok,
+
+----I am going to start exploring Chapter 10 in the SQL book.
+----The SQL book is " Microsoft SQL Server 2016 : A Beginner's Guide "
+
+--Chapter 10 :
+
+--Indices:
+
+----I believe that Indices will help me solve the Median problem I am having.
+
+--Introduction
+--Transact-SQL and indices.
+--Guidelines for Creating and Using Indices.
+--Special Types of Indices.
+
+----I am excited for all of this.
+
+--This chapter describes indices and their role in optimizing the response time of queries.
+
+--The fisr part of the chapter discusses how indices are stored and the existing form of them.
+
+----Perfect! I believe that this covers it.
+----becuase I was wondering that I might not be able to make an index on the HackerRank interface.
+----I know previously I had problems in solving another problem
+----where I wanted to create a whole new column.
+----which made me have to get better at nested functions.
+----so, if there already are "existing forms of them".
+----"them" being indices.
+----then, I might not need to add a whole new index_column.
+
+--The main part of the chapter
+--explains three Transact-SQL statements pertaining to indices:
+--CREATE INDEX,
+--ALTER INDEX,
+--and
+--DROP INDEX.
+
+----ok,
+----I am really hoping I can either use an already existing column as an index.
+----if there isn't an already existing one.
+----why wouldn't there already be an existing index?
+
+--After that,
+--index fragmentation and its impact
+--on the performance of the system will be explained.
+
+--The next part of the chapter gives you several general recommendations
+--for how and when indices should be created.
+
+----I am also exited for that part of the chapter.
+----I am sure there are many reasons to create an idex.
+
+--The final part of the chapter describes several special types of indices.
+
+--INTRODUCTION:
+
+--Databas systems generally use indices to provide fast access to relational data.
+--an index is a separate physical data structure that enables queries to access one of more data rows fast.
+
+----I am getting discouraged that maybe 
+----I think I found something.
+----I am exited.
