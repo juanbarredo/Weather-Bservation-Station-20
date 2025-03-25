26c21e48b25d15b82752ec8fd458d9a073635728
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-03-25 19:49:08 -0400

    03 25 2025
    I am very excited because I am learning how the INDEX works in TSQL.
    
    I am excited but I am afraid that there might not be a way access the Table Scan.  I can be assured that TSQL is using the table scan when I have not or there is not an index access.
    
    so, but how do I tell it to count and the n stop half way after done scanning once?

--- DETOUR TO CHAPTER 10 Indices.sql
+++ DETOUR TO CHAPTER 10 Indices.sql
@@ -63,3 +63,43 @@
 ----I am getting discouraged that maybe 
 ----I think I found something.
 ----I am exited.
+
+----------------------------------------------03 25 2025----------------------------------------------------
+
+----let's see how athis goes.
+
+--Proper tuning of indices is therefore a key for good query perfomarnce.
+
+--An index is in many ways analogous to a book index.
+--When you are looking for a topic in a book,
+--you use its index to find the pages where that topoc is described.
+--Similarly,
+--when you search for a row of a table, ----how is this done in their mind?  
+--the Database Engine uses an index
+--to find its physical location.
+--However, there are two main differences 
+--between a book index and a database index:
+
+--	A s a book reader,
+--	you can decide whether or not to use the book's index.
+--	This possibility generally does not exist if you use a database system:
+--	the system component called the query optimizer decides whethefr or not to use an existing index.
+
+----right, an "existing index"
+
+--	A particular book's index is edited together with the book
+--	and does not change at all.
+--	This means that you can find a topic exactly on the page where it is determined in the index.
+--	In contrast,
+--	a database index can change each time the corresponding data is changed.
+
+--If a table does not have an appropriate index,
+--the database system uses the table scan method to retrieve rows.
+--Table Scan means that each row is retrieved and examined in sequence.
+--( from first to last )
+--and returned in the result set f the search condition
+--in the WHERE clause evaluates to TRUE.
+--Therefore,
+--all rows are fetched according to their physical memory location.
+
+--This method is less efficient than an index access, as explained next.
